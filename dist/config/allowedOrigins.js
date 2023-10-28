8f28cda16701e616a54291c8d99c23341b89c030
Jelon <86931022+Jelon1211@users.noreply.github.com>
2023-10-28 18:23:34 +0200

    Delete dist directory

--- dist/config/allowedOrigins.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedOrigins = void 0;
-const allowedOrigins = ["http://localhost:3000"];
-exports.allowedOrigins = allowedOrigins;
--- dist/src/routes/root.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-const path_1 = require("path");
-router.get("^/$|index(.html)?", (req, res) => {
-    res.sendFile((0, path_1.join)(__dirname, "..", "views", "index.html"));
-});
-exports.default = router;
